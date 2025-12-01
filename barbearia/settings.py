4acc8dfd93260393a7595b8269e2be8a2f8bc500
marco antonio <2msoftware@gmail.com>
2025-11-30 22:16:46 -0300

    integrando o css de volta

--- barbearia/settings.py
+++ barbearia/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_v972$d$8gozcoki8vz$e3mlpko1)djx5_v5#g2%+p#ls#ba@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'barbearia' / 'static']
 
 ADMIN_BASIC_USER = 'admin'
